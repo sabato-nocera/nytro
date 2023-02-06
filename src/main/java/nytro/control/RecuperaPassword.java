1f951d70e5625d01407c4f82c73d64f483ccf014
sabato-nocera <nocera.sabato@gmail.com>
2023-02-06 10:16:44 +0100

    Update

--- src/main/java/nytro/control/RecuperaPassword.java
+++ src/main/java/nytro/control/RecuperaPassword.java
@@ -27,53 +27,47 @@ public class RecuperaPassword extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String emailRecupero = request.getParameter("emailRecupero");
+		String password = request.getParameter("password");
+		String passwordConferma = request.getParameter("passwordConferma");
+
+		AccountBean account = null;
+		System.out.println(username);
+
+		if(username==null || emailRecupero==null || username.equals("") || emailRecupero.equals("") || password==null || passwordConferma==null || password.equals("") || passwordConferma.equals(""))
+			throw new MyException("Username e/o email di recupero vuota.");
+
+		if(!password.equals(passwordConferma))
+			throw new MyException("Campo password differente dal campo password conferma.");
 
 		try {
-			String username = request.getParameter("username");
-			String emailRecupero = request.getParameter("emailRecupero");
-			String password = request.getParameter("password");
-			String passwordConferma = request.getParameter("passwordConferma");
-
-			AccountBean account = null;
-			System.out.println(username);
-			if(username==null || emailRecupero==null || username.equals("") || emailRecupero.equals("") || password==null || passwordConferma==null || password.equals("") || passwordConferma.equals(""))
-				throw new MyException("Username e/o email di recupero vuota.");
-
-			if(!password.equals(passwordConferma))
-				throw new MyException("Campo password differente dal campo password conferma.");
-
-			try {
-				account = accountDAO.doRetrieveByUsername(username);
-			} catch (SQLException e) {
-				throw new MyException("Errore estrazione account.");
-			}
-
-			if(account.getUsername()==null || account.getUsername().equals(""))
-				throw new MyException("Account assente");
-
-			if(!account.getEmailRecupero().equals(emailRecupero))
-				throw new MyException("Email di recupero inserita non valida: " + account.getEmailRecupero() +" " + emailRecupero);
-
-			password = Utils.generatePwd(password);
-			account.setPassword(password);
-
-			try {
-				accountDAO.doUpdate(account);
-			} catch (SQLException e) {
-				throw new MyException("Fallimento cambiamento password");
-			}
-
-			String message="Password aggiornata con successo.";
-
-			request.setAttribute("message", message);
-
-			String url = response.encodeURL("jsp/loginForm.jsp");
-			request.getRequestDispatcher(url).forward(request, response);
-
-		} catch (MyException e) {
-
+			account = accountDAO.doRetrieveByUsername(username);
+		} catch (SQLException e) {
+			throw new MyException("Errore estrazione account.");
 		}
 
+		if(account.getUsername()==null || account.getUsername().equals(""))
+			throw new MyException("Account assente");
+
+		if(!account.getEmailRecupero().equals(emailRecupero))
+			throw new MyException("Email di recupero inserita non valida: " + account.getEmailRecupero() +" " + emailRecupero);
+
+		password = Utils.generatePwd(password);
+        account.setPassword(password);
+
+		try {
+			accountDAO.doUpdate(account);
+		} catch (SQLException e) {
+			throw new MyException("Fallimento cambiamento password");
+		}
+
+		String message="Password aggiornata con successo.";
+
+		request.setAttribute("message", message);
+
+		String url = response.encodeURL("jsp/loginForm.jsp");
+		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 	
